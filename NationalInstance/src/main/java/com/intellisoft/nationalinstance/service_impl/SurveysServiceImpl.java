bb1d88053625aab0310e83988849cb1142763463
David <davidnjau21@gmail.com>
2023-03-17 11:56:54 +0300

    Status values

--- NationalInstance/src/main/java/com/intellisoft/nationalinstance/service_impl/SurveysServiceImpl.java
+++ NationalInstance/src/main/java/com/intellisoft/nationalinstance/service_impl/SurveysServiceImpl.java
@@ -46,8 +46,13 @@ public class SurveysServiceImpl implements SurveysService{
 
     @Override
     public Results listAdminSurveys(String creatorId, String status) {
+        List<Surveys> surveysList = new ArrayList<>();
+        if (status.equals("ALL")){
+            surveysList = surveysRepo.findAllByCreatorId(creatorId);
+        }else {
+            surveysList = surveysRepo.findByCreatorIdAndStatus(creatorId, status);
 
-        List<Surveys> surveysList = surveysRepo.findByCreatorIdAndStatus(creatorId, status);
+        }
         DbResults dbResults = new DbResults(
                 surveysList.size(),
                 surveysList);
@@ -58,7 +63,13 @@ public class SurveysServiceImpl implements SurveysService{
     @Override
     public Results listRespondentsSurveys(String creatorId, String status) {
         List<DbSurveyRespondentDetails> dbSurveyRespondentDetailsList = new ArrayList<>();
-        List<Surveys> surveysList = surveysRepo.findByCreatorIdAndStatus(creatorId,status);
+        List<Surveys> surveysList = new ArrayList<>();
+        if (status.equals("ALL")){
+            surveysList = surveysRepo.findAllByCreatorId(creatorId);
+        }else {
+            surveysList = surveysRepo.findByCreatorIdAndStatus(creatorId, status);
+
+        }
         for (int i = 0; i < surveysList.size(); i++){
 
             String surveyId = String.valueOf(surveysList.get(i).getId());
