84c224b617b0f4c7946ad1f2d4e0a55890d60ee0
nelsonkimaiga <nelsonkimaiga@aol.com>
2023-05-12 12:30:38 +0300

    Retrieve routine submissions feature

--- PssNationalInstance/src/main/java/com/intellisoft/pssnationalinstance/db/DataEntry.java
+++ PssNationalInstance/src/main/java/com/intellisoft/pssnationalinstance/db/DataEntry.java
@@ -23,7 +23,11 @@ public class DataEntry {
     private String orgUnit;
     private String selectedPeriod;
     private String status; // DRAFT / SUBMITTED
-    private String dataEntryPersonId; //The person
+    private String dataEntryPersonId; //The person's user id
+    private String username; //The Person's username
+    private String firstName; //The Person's firstName
+    private String surname; //The Person's surname
+    private String email; //The Person's email
     private String dataEntryDate; // The format is yyyymmdd
     private String versionNumber; // Current version number
     @CreationTimestamp
--- PssNationalInstance/src/main/java/com/intellisoft/pssnationalinstance/repository/DataEntryRepository.java
+++ PssNationalInstance/src/main/java/com/intellisoft/pssnationalinstance/repository/DataEntryRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DataEntryRepository extends CrudRepository<DataEntry, Long> {
-    Page<DataEntry> findAllByStatusAndDataEntryPersonId(String status, String dataEntryPersonId, Pageable pageable);
-    Page<DataEntry> findAllByDataEntryPersonId(String dataEntryPersonId, Pageable pageable);
+    Page<DataEntry> findAllByStatus(String status, Pageable pageable);
+    Page<DataEntry> findAll(Pageable pageable);
 
 }
