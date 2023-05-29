ec63f102a3c40cafea95492c9b08ee3405dee300
nelsonkimaiga <nelsonkimaiga@aol.com>
2023-05-29 13:52:51 +0300

    PSS-416 bug fix -> load data entry submisions based on logged in data entry user

--- PssNationalInstance/src/main/java/com/intellisoft/pssnationalinstance/repository/DataEntryRepository.java
+++ PssNationalInstance/src/main/java/com/intellisoft/pssnationalinstance/repository/DataEntryRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DataEntryRepository extends CrudRepository<DataEntry, Long> {
     Page<DataEntry> findAllByStatus(String status, Pageable pageable);
     Page<DataEntry> findAll(Pageable pageable);
+    Page<DataEntry> findAllByStatusAndDataEntryPersonId(String status, String dataEntryPersonId, Pageable pageable);
 
+    List<DataEntry> findByDataEntryPersonId(String dataEntryPersonId);
+    Page<DataEntry> findAllByDataEntryPersonId(String dataEntryPersonId, Pageable pageable);
 }
